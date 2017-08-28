ac4cb80afd2ef5f484738ebb128a07aebb20f52f
Tomasz Mlynski <devilshakerz@gmail.com>
2017-08-28 16:54:53 +0200

    Move configuration to dotenv format

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+var dotenv = require('dotenv').config();
 var config = require('./config');
 var wsredisServer = require('./wsredisServer');
 
--- config.js
+++ config.js
@@ -1,22 +1,20 @@
 var config = {
     // WebSockets server
-    "port": 8080,
-    "tokenKey": "CHANGE_ME",
-    "allowedOrigins": [
-        "http://localhost"
-    ],
+    "port": process.env.WSREDIS_PORT,
+    "tokenKey": process.env.WSREDIS_TOKEN_KEY,
+    "allowedOrigins": process.env.WSREDIS_ALLOWED_ORIGINS.split(','),
 
     // Redis client
-    "redisHostname": "localhost",
-    "redisPort": 6379,
+    "redisHostname": process.env.WSREDIS_REDIS_HOSTNAME,
+    "redisPort": process.env.WSREDIS_REDIS_PORT,
 
     // interal & debug
     "jwtVerifyOptions": {
-        algorithms: ["HS256", "HS512"],
-        maxAge: "10m",
-        clockTolerance: 10,
+        algorithms: process.env.WSREDIS_JWT_ALGORITHMS.split(','),
+        maxAge: process.env.WSREDIS_JWT_MAX_AGE,
+        clockTolerance: process.env.WSREDIS_JWT_CLOCK_TOLERANCE,
     },
-    "verbosity_level": 1,
+    "verbosity_level": process.env.WSREDIS_VERBOSITY_LEVEL,
 };
 
 module.exports = config;
