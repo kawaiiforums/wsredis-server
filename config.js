c6fc9b18d3ccfcf3fa7d07400c3c773ec5db1914
Tomasz Mlynski <devilshakerz@gmail.com>
2017-09-29 19:39:02 +0200

    Add configuration values type casting

--- config.js
+++ config.js
@@ -1,21 +1,21 @@
 var config = {
     // WebSockets server
-    "port": process.env.WSREDIS_PORT,
+    "port": parseInt(process.env.WSREDIS_PORT),
     "tokenKey": process.env.WSREDIS_TOKEN_KEY,
     "allowedOrigins": process.env.WSREDIS_ALLOWED_ORIGINS.split(','),
-    "keepaliveInterval": process.env.WSREDIS_KEEPALIVE_INTERVAL,
+    "keepaliveInterval": parseInt(process.env.WSREDIS_KEEPALIVE_INTERVAL),
 
     // Redis client
     "redisHostname": process.env.WSREDIS_REDIS_HOSTNAME,
-    "redisPort": process.env.WSREDIS_REDIS_PORT,
+    "redisPort": parseInt(process.env.WSREDIS_REDIS_PORT),
 
     // interal & debug
     "jwtVerifyOptions": {
         algorithms: process.env.WSREDIS_JWT_ALGORITHMS.split(','),
         maxAge: process.env.WSREDIS_JWT_MAX_AGE,
-        clockTolerance: process.env.WSREDIS_JWT_CLOCK_TOLERANCE,
+        clockTolerance: parseInt(process.env.WSREDIS_JWT_CLOCK_TOLERANCE),
     },
-    "verbosity_level": process.env.WSREDIS_VERBOSITY_LEVEL,
+    "verbosity_level": parseInt(process.env.WSREDIS_VERBOSITY_LEVEL),
 };
 
 module.exports = config;
